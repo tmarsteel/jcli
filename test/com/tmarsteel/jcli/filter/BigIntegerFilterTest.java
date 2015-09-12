aabd7cbd61ed387630a8c0714a386eda7918c4e4
Tobias Marstaller <tobias.marstaller@gmail.com>
2015-09-12 17:34:26 +0200

    Unit-Test for XML-Constructor of com.tmarsteel.jcli.filter.BigIntegerFilter

--- test/com/tmarsteel/jcli/filter/BigIntegerFilterTest.java
+++ test/com/tmarsteel/jcli/filter/BigIntegerFilterTest.java
@@ -1,9 +1,18 @@
 package com.tmarsteel.jcli.filter;
 
+import com.tmarsteel.jcli.ParseException;
 import com.tmarsteel.jcli.validation.ValidationException;
+import java.io.IOException;
 import java.math.BigInteger;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import static org.junit.Assert.*;
+import org.junit.Before;
 import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 /**
  * @author Tobias Marstaller
@@ -92,5 +101,68 @@ public class BigIntegerFilterTest
         String number = "10938209380aaaaa";
         
         Object ret = bdf.parse(number);
+    }
+    
+    // --------------------
+    
+    private NodeList testNodes;
+    
+    @Before
+    public void setUp()
+        throws ParserConfigurationException, SAXException, IOException
+    {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        dbf.setValidating(false);
+        dbf.setIgnoringComments(true);
+        dbf.setIgnoringElementContentWhitespace(true);
+        
+        DocumentBuilder builder = dbf.newDocumentBuilder();
+        
+        Document testDocument = builder.parse(getClass().getResourceAsStream("BigIntegerFilterTest.xml"));
+        
+        testNodes = testDocument.getElementsByTagName("filter");
+    }
+    
+    @Test
+    public void testNodeConstructor()
+        throws ParseException
+    {
+        if (testNodes == null)
+        {
+            fail("The test-document could not be loaded. See errors for #setUp()");
+        }
+        
+        BigIntegerFilter filter = new BigIntegerFilter(testNodes.item(0));
+        
+        assertEquals(filter.getMinValue().toString(), "10");
+        assertEquals(filter.getMaxValue().toString(), "2000");
+    }
+    
+    @Test
+    public void testNodeConstructorWithRadix()
+        throws ParseException
+    {
+        if (testNodes == null)
+        {
+            fail("The test-document could not be loaded. See errors for #setUp()");
+        }
+        
+        BigIntegerFilter filter = new BigIntegerFilter(testNodes.item(1));
+        
+        assertEquals(filter.getMinValue().toString(10), "10");
+        assertEquals(filter.getMaxValue().toString(10), "2000");
+        assertEquals(filter.getRadix(), 16);
+    }
+    
+    @Test(expected = NumberFormatException.class)
+    public void nodeConstructorShouldFailOnNonNumerical()
+        throws ParseException
+    {
+        if (testNodes == null)
+        {
+            fail("The test-document could not be loaded. See errors for #setUp()");
+        }
+
+        BigIntegerFilter filter = new BigIntegerFilter(testNodes.item(2));
     }
 }
