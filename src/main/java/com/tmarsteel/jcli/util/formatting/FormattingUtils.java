149cc56246f12f3b8b5dcb82ac26ab243f283418
Tobias Marstaller <tobias.marstaller@gmail.com>
2017-06-09 21:11:49 +0200

    #11: Adds FormattingUtils#padLeft(String, int) and #padLeftRenderable(String)

--- src/main/java/com/tmarsteel/jcli/util/formatting/FormattingUtils.java
+++ src/main/java/com/tmarsteel/jcli/util/formatting/FormattingUtils.java
@@ -27,6 +27,30 @@ public final class FormattingUtils
         return new String(target);
     }
 
+    /**
+     * Prepends space characters ({@code ' '}) to the start of the given string to make the string of the given target
+     * length. Returns that string. If the given string is as long or longer than the target length the given string
+     * is returned without any modification.
+     * @return The padded string, e.g. for inputs "abc" and 6 returns "   abc"
+     */
+    public static String padLeft(String toBePadded, int nSpaces)
+    {
+        return (new String(new char[nSpaces]).replace('\0', ' ')) + toBePadded;
+    }
+
+    /**
+     * Returns a {@link Renderable} that right-aligns the given string when rendering. Does not support wrapping the
+     * text because that is a responsibility of {@link com.tmarsteel.jcli.util.formatting.multiline.MultilineTextStrategy}.
+     */
+    public static Renderable padLeftRenderable(String content) {
+        return (maxWidth, lineSeparator) -> {
+            if (content.length() > maxWidth) {
+                throw new OutOfRenderingSpaceException("Wrapping + right align not supported yet.");
+            }
+            return padLeft(content, maxWidth);
+        };
+    }
+
     public static <T> String join(T[] items, String glue) {
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0;i < items.length;i++) {
