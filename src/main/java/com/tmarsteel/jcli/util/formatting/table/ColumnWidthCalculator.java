17e2f23cc3a1aa06872d27ab463d004ef778b0d8
Tobias Marstaller <tobias.marstaller@gmail.com>
2017-06-09 21:11:17 +0200

    #11: Adds static ColumnWidthCalculator#ofValues(int...)

--- src/main/java/com/tmarsteel/jcli/util/formatting/table/ColumnWidthCalculator.java
+++ src/main/java/com/tmarsteel/jcli/util/formatting/table/ColumnWidthCalculator.java
@@ -3,6 +3,23 @@ package com.tmarsteel.jcli.util.formatting.table;
 public interface ColumnWidthCalculator
 {
     /**
+     * Returns a {@link ColumnWidthCalculator} that returns the given values for the columns with the
+     * respective index. E.g. the calculator resulting from an invocation of {@code ofValues(10, 20, 30)}
+     * returns the columns widths 10, 20 and 30 for the column indexes 0, 1 and 2 respectively.
+     */
+    static ColumnWidthCalculator ofValues(int... values) {
+        return (cIndex, nCs) -> {
+            try {
+                return values[cIndex];
+            }
+            catch (ArrayIndexOutOfBoundsException ex)
+            {
+                throw new UnsupportedOperationException("Column #" + cIndex + " not defined", ex);
+            }
+        };
+    }
+
+    /**
      * Given the total number of columns in a table and the index of one particular column, returns the desired
      * width of that column.
      *
