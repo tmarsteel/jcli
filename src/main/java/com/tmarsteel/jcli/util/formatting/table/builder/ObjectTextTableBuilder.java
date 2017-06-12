b2715d68d633241726a8699d527cff982ef839e1
Tobias Marstaller <tobias.marstaller@gmail.com>
2017-06-13 00:42:38 +0200

    #17: Make ObjecTextTableBuilder a class so that the actual implementations can be hidden

--- src/main/java/com/tmarsteel/jcli/util/formatting/table/builder/ObjectTextTableBuilder.java
+++ src/main/java/com/tmarsteel/jcli/util/formatting/table/builder/ObjectTextTableBuilder.java
@@ -8,7 +8,7 @@ import java.util.Collection;
  * Groups {@link TextTableBuilder}s that can take a {@link Collection}<? extends ObjectType> and spit out a nice
  * {@link TextTable}.
  */
-public interface ObjectTextTableBuilder<ObjectType>
+public abstract class ObjectTextTableBuilder<ObjectType> extends TextTableBuilder<ObjectTextTableBuilder<ObjectType>>
 {
-    TextTable build(Collection<? extends ObjectType> items);
+    public abstract TextTable build(Collection<? extends ObjectType> items);
 }
